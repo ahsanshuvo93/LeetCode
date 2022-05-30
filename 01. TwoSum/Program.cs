c7026fa1bbce952ba5584bec0e8055a636791ac9
Ahsan Shuvo <ahsan_003@outlook.com>
2022-05-30 23:47:41 +0600

    01._TwoSum My Solution

---
+++ 01. TwoSum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _01._TwoSum
+{
+    class Program
+    {
+        // Date : 26/May/2022
+        // Problem Name : Two Sum
+        // Topic : Array/String
+        // URL : https://leetcode.com/problems/two-sum/
+        // Status : My Solution
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int[] intArray = new int[] { 2, 7, 11, 15 };
+            //Console.WriteLine(TwoSum(intArray, 9));
+
+            var result = TwoSum(intArray, 9);
+
+            string output = String.Empty;
+
+            foreach (var res in result)
+            {
+                output = output + res + ",";
+            }
+
+            output = output.Remove(output.Length - 1, 1);
+
+
+            Console.WriteLine(output);
+
+            Console.ReadKey();
+        }
+
+
+        // Method of Two Sum
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            //Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return nums;
+        }
+    }
+}
