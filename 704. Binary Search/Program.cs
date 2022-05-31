8384b2d562766afdac3e490c7a3d0bb064abe0a1
Ahsan Shuvo <ahsan_003@outlook.com>
2022-06-01 00:41:28 +0600

    704. Binary Search

---
+++ 704. Binary Search/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _704._Binary_Search
+{
+    class Program
+    {
+        // Date : 31/May/2022
+        // Problem Name : 704. Binary Search
+        // Topic : Binary Search
+        // URL : https://leetcode.com/problems/binary-search/
+        // Status : My Solution
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int[] array = new int[] { -1, 0, 5 };
+            int target = 50;
+
+            Console.WriteLine(BinarySearch(array, target));
+
+            Console.ReadKey();
+        }
+
+        static int BinarySearch(int[] nums, int target)
+        {
+            try
+            {
+                int L = 0;
+                int R = nums.Length - 1;
+
+                while (L < R)
+                {
+                    int M = (L + R) / 2;
+
+                    if (nums[M] < target)
+                    {
+                        L = M + 1;
+                    }
+                    else 
+                    {
+                        R = M;
+                    }
+                    
+                }
+                return (L == R && nums[L] == target) ? L : -1;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
