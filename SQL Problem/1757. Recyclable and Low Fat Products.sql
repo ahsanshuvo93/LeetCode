7ddd4a63ecb78cdec35bdac188642033ad0b491b
Ahsan Shuvo <ahsan_003@outlook.com>
2022-10-31 23:43:15 +0600

    1757. Recyclable and Low Fat Products

---
+++ SQL Problem/1757. Recyclable and Low Fat Products.sql
@@ -0,0 +1,10 @@
+﻿
+--Date : 31/October/2022
+--Problem Name : 1757. Recyclable and Low Fat Products
+--Topic : SQL
+--URL : https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan&id=sql-i
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Products
+select product_id from Products where low_fats >= 'Y' and recyclable >= 'Y'
