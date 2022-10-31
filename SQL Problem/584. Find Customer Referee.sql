da5ba51f48a7a09c28bdefeed59b9f1c1dd2e34b
Ahsan Shuvo <ahsan_003@outlook.com>
2022-10-31 23:56:48 +0600

    584. Find Customer Referee

---
+++ SQL Problem/584. Find Customer Referee.sql
@@ -0,0 +1,10 @@
+﻿
+--Date : 31/October/2022
+--Problem Name : 584. Find Customer Referee
+--Topic : SQL
+--URL : https://leetcode.com/problems/find-customer-referee/?envType=study-plan&id=sql-i
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Customer
+select name from Customer where referee_id != 2 or referee_id is null
