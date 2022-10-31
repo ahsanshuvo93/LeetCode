754a6356b410a57e588e505edaa43076d5c57494
Ahsan Shuvo <ahsan_003@outlook.com>
2022-11-01 00:04:44 +0600

    183. Customers Who Never Order

---
+++ SQL Problem/183. Customers Who Never Order.sql
@@ -0,0 +1,12 @@
+﻿
+--Date : 31/October/2022
+--Problem Name : 183. Customers Who Never Order
+--Topic : SQL
+--URL : https://leetcode.com/problems/customers-who-never-order/?envType=study-plan&id=sql-i
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Customers
+--Table Name : Orders
+
+select name as Customers from Customers where id not in (select customerId from Orders)
