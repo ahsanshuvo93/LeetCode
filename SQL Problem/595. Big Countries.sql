9bf88145b4be88768e2e0a4105b87bf9daa408b5
Ahsan Shuvo <ahsan_003@outlook.com>
2022-10-31 23:24:56 +0600

    595. Big Countries

---
+++ SQL Problem/595. Big Countries.sql
@@ -0,0 +1,10 @@
+﻿
+--Date : 31/October/2022
+--Problem Name : 595. Big Countries
+--Topic : SQL
+--URL : https://leetcode.com/problems/big-countries/?envType=study-plan&id=sql-i
+--Status : My Solution
+--Level : Easy
+
+--Table Name : World
+select name, population, area from World where area >= 3000000 or population >= 25000000
