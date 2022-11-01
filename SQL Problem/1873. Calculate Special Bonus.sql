7d0bdba17b20e889232d7210fdb3dd7befde7d4c
Ahsan Shuvo <ahsan_003@outlook.com>
2022-11-01 20:33:34 +0600

    1873. Calculate Special Bonus

---
+++ SQL Problem/1873. Calculate Special Bonus.sql
@@ -0,0 +1,19 @@
+﻿
+--Date : 01/November/2022
+--Problem Name : 1873. Calculate Special Bonus
+--Topic : SQL (Select, Order)
+--URL : https://leetcode.com/problems/calculate-special-bonus/?envType=study-plan&id=sql-i
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Employees
+
+select employee_id, 
+	case
+		when (employee_id % 2 = 0 or LEFT(name, 1) = 'M') then 0
+		else salary
+	end as Bonus
+from Employees
+order by employee_id
+
+--LEFT(name, 1) ==>> Take the first character from string
