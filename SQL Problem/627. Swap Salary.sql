09c8f313b2053997d1a4e15c4c970fcdabf6a5ef
Ahsan Shuvo <ahsan_003@outlook.com>
2022-11-01 20:56:13 +0600

    627. Swap Salary

---
+++ SQL Problem/627. Swap Salary.sql
@@ -0,0 +1,15 @@
+﻿
+--Date : 01/November/2022
+--Problem Name : 627. Swap Salary
+--Topic : SQL
+--URL : https://leetcode.com/problems/swap-salary/?envType=study-plan&id=sql-i
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Salary
+
+update Salary set sex = 
+    case
+        when sex = 'm' then 'f'
+        when sex = 'f' then 'm'
+    end
