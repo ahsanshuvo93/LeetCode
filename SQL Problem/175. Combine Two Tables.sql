da18656b9f7af9bb39b3f4058786accbb7aee1df
Ahsan Shuvo <ahsan_003@outlook.com>
2022-11-03 22:58:23 +0600

    175. Combine Two Tables

---
+++ SQL Problem/175. Combine Two Tables.sql
@@ -0,0 +1,14 @@
+﻿
+--Date : 03/November/2022
+--Problem Name : 175. Combine Two Tables
+--Topic : SQL (Join)
+--URL : https://leetcode.com/problems/combine-two-tables/
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Person
+--Table Name : Address
+
+select per.firstName, per.lastName, adr.city, adr.State from Person as per
+left join Address as adr on
+per.PersonId = adr.PersonId
