06e3bbf6b9a11a50393b41f88c6e0b4bc83038c1
Ahsan Shuvo <ahsan_003@outlook.com>
2022-11-07 23:27:08 +0600

    1667. Fix Names in a Table

---
+++ SQL Problem/1667. Fix Names in a Table.sql
@@ -0,0 +1,15 @@
+﻿
+--Date : 07/November/2022
+--Problem Name : 1667. Fix Names in a Table
+--Topic : SQL String Process
+--URL : https://leetcode.com/problems/fix-names-in-a-table/?envType=study-plan&id=sql-i
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Users
+
+select 
+	user_id, 
+	CONCAT(UPPER(LEFT(name,1)), LOWER(RIGHT(name,LEN(name)-1))) as name 
+from Users 
+order by user_id
