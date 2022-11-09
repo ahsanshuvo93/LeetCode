cbcf86ef5f4fdd6ae29362b09b72849ddc4383f3
Ahsan Shuvo <ahsan_003@outlook.com>
2022-11-09 22:18:35 +0600

    196. Delete Duplicate Emails

---
+++ SQL Problem/196. Delete Duplicate Emails.sql
@@ -0,0 +1,16 @@
+﻿
+--Date : 09/November/2022
+--Problem Name : 196. Delete Duplicate Emails
+--Topic : SQL
+--URL : https://leetcode.com/problems/delete-duplicate-emails/
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Person
+
+with CTE_tempTable (id, email, num)
+as
+(
+	select id, email, ROW_NUMBER() over(Partition BY email order by id) as num from Person
+)
+delete from CTE_tempTable where num > 1
