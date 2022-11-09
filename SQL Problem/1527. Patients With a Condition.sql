a8e5d09669e72c2eb0ebbd91b2356c2b68da94f7
Ahsan Shuvo <ahsan_003@outlook.com>
2022-11-09 20:55:45 +0600

    1527. Patients With a Condition

---
+++ SQL Problem/1527. Patients With a Condition.sql
@@ -0,0 +1,21 @@
+﻿
+--Date : 09/November/2022
+--Problem Name : 1527. Patients With a Condition
+--Topic : SQL (String Processing)
+--URL : https://leetcode.com/problems/patients-with-a-condition/
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Patients
+
+with CTE_tempTable (patient_id, patient_name, conditions)
+as
+(
+	select patient_id, patient_name,
+		case
+			when conditions like 'DIAB1%' then conditions
+			when conditions like '% DIAB1%' then conditions
+		end as conditions
+	from Patients
+)
+select * from CTE_tempTable where conditions is not null
