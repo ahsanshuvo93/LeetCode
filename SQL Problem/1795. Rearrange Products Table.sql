a463f4b53b54c6b4f9bc51a4c399f620728acd85
Ahsan Shuvo <ahsan_003@outlook.com>
2022-11-16 21:45:35 +0600

    1795. Rearrange Products Table

---
+++ SQL Problem/1795. Rearrange Products Table.sql
@@ -0,0 +1,18 @@
+﻿
+--Date : 16/November/2022
+--Problem Name : 1795. Rearrange Products Table
+--Topic : SQL (Select & Union)
+--URL : https://leetcode.com/problems/rearrange-products-table/
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Products
+
+--UNPIVOT is used to Rotate multiple columns into multiple rows
+
+select u.product_id, u.store, u.price  from Products s
+UNPIVOT
+(
+    price for store in (store1, store2, store3)
+)
+as u
