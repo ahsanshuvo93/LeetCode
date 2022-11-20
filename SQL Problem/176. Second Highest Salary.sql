45249b44879dcafec39431a0b89bde4e910f00fd
Ahsan Shuvo <ahsan_003@outlook.com>
2022-11-20 22:58:30 +0600

    176. Second Highest Salary

---
+++ SQL Problem/176. Second Highest Salary.sql
@@ -0,0 +1,18 @@
+﻿
+--Date : 20/November/2022
+--Problem Name : 176. Second Highest Salary
+--Topic : SQL
+--URL : https://leetcode.com/problems/second-highest-salary/
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Employee
+
+with CTE_RakingTable(salary, serial)
+as
+(
+select salary, Rank() over(order by salary desc) as serial from Employee group by salary 
+)
+select Isnull((select salary from CTE_RakingTable where serial = 2),null) as SecondHighestSalary
+
+
