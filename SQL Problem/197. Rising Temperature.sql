375c70ff0aa751a04d0ec456832c101d70b5aedb
Ahsan Shuvo <ahsan_003@outlook.com>
2022-12-01 23:07:33 +0600

    197. Rising Temperature

---
+++ SQL Problem/197. Rising Temperature.sql
@@ -0,0 +1,25 @@
+﻿
+--Date : 02/December/2022
+--Problem Name : 197. Rising Temperature
+--Topic : SQL
+--URL : https://leetcode.com/problems/rising-temperature/
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Weather
+
+with CTE_TemperatureWithPreviousData(id, temperature, recordDate, prev_temperature, prev_recordDate)
+as
+(
+    select id, temperature, recordDate, lag(temperature,1) over (order by recordDate) as prev_temperature, lag(recordDate,1) over(order by recordDate) as prev_recordDate from Weather 
+),
+CTE_TempTable(id,temperature,recordDate)
+as
+(
+    select id, temperature,
+        case
+            when (temperature > prev_temperature)  and (recordDate = DATEADD(day, 1, prev_recordDate)) then recordDate
+        end as recordDate
+    from CTE_TemperatureWithPreviousData
+)
+select id from CTE_TempTable where recordDate is not null
