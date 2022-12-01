f2394ec218a85b44d1322267104ad498fd9e4fcf
Ahsan Shuvo <ahsan_003@outlook.com>
2022-12-01 21:00:00 +0600

    1581. Customer Who Visited but Did Not Make Any Transactions

---
+++ SQL Problem/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,19 @@
+﻿
+--Date : 02/December/2022
+--Problem Name : 1581. Customer Who Visited but Did Not Make Any Transactions
+--Topic : SQL
+--URL : https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Visits
+--Table Name : Transactions
+
+with CTE_CustomerVisitWithoutTransaction(customer_id)
+as
+(
+	select v.customer_id as customerId from Visits as v
+	left join Transactions as t 
+	on t.visit_id = v.visit_id where amount is null
+)
+select customer_id, count(customer_id) as count_no_trans from CTE_CustomerVisitWithoutTransaction group by customer_id
