0cfdbcb8ccb6462d10503721f20f8709b270b0ad
Ahsan Shuvo <ahsan_003@outlook.com>
2022-12-01 21:46:09 +0600

    1148. Article Views I

---
+++ SQL Problem/1148. Article Views I.sql
@@ -0,0 +1,20 @@
+﻿
+--Date : 02/December/2022
+--Problem Name : 1148. Article Views I
+--Topic : SQL
+--URL : https://leetcode.com/problems/article-views-i/
+--Status : My Solution
+--Level : Easy
+
+--Table Name : Views
+
+with CTE_OwnView (author_id, viewer_id)
+as
+(
+	select author_id,
+		case
+			when author_id = viewer_id then viewer_id
+		end as viewer_id
+	from Views
+)
+select Distinct (author_id) as id from CTE_OwnView where viewer_id is not null order by author_id
